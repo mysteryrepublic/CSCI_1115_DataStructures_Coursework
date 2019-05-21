efccf0af8c10f23dba121d3e72ab60a15994e875
mysteryrepublic <50370966+mysteryrepublic@users.noreply.github.com>
2019-05-21 10:24:05 -0600

    Chapter 27 assignment files

---
+++ Chapter 27/Exercise27_09.java
@@ -0,0 +1,28 @@
+/* 
+Shannon Vogelesang
+Date:05-21-2019
+Description: (Implement hashCode for string) Write a method that returns a hashcode for string using the approach described in 
+			Section 27.3.2 with b value 31. Test class provided
+
+Given: "4.5"      Expected: 51451
+Given: "Hello"    Expected: 69609650
+ */
+public class Exercise27_09 { 
+	public static void main(String[] args) {
+		String s = "4.5";
+		System.out.println(hashCodeForString(s));
+
+		s = "Hello";
+		System.out.println(hashCodeForString(s));
+	}
+
+	public static int hashCodeForString(String s) {
+		// Add your code here
+		int b = 31;
+		int x = 0;
+		for (int i = 0; i < s.length(); i++) {
+			x = x * b + s.charAt(i);
+		}
+		return x;
+	}
+}
---
+++ Chapter 27/Exercise27_11.java
@@ -0,0 +1,33 @@
+/*
+ * Shannon Vogelesang
+ * 05-21-2019
+ * 
+ * Description: (setToList) Write the following method that returns an ArrayList from a set. Test class provided
+ * 
+ * Remember that a set will not conain duplicates
+ * 
+ * 
+ * */
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Exercise27_11 {
+	public static void main(String[] args) {
+		Set<String> set = new HashSet<String>(); 
+		set.add("Smith");
+		set.add("Anderson");
+		set.add("Lewis");
+		set.add("Cook"); 
+		set.add("Smith");
+
+		ArrayList<String> list = setToList(set);
+		System.out.println(list);
+	}
+
+	public static <E> ArrayList<E> setToList(Set<E> s) {
+		 ArrayList<E> list = new ArrayList<E>();
+		    list.addAll(s);
+		    return list;
+	}
+}
